760cfc6ef02f8be82ad5b7910c425c6b677d3ab0
dude lao <brycelipscomb@gmail.com>
2020-11-06 13:51:22 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+
+import Characters from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +13,53 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [chars, setChars] = useState([]);
+
+  useEffect(() => {
+    const fetchChars = () => {
+      axios.get(`https://rickandmortyapi.com/api/character/`)
+      .then((res) => {
+        setChars(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((err) => {
+
+      });
+    }
+    fetchChars();
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <MainContain>
+        <Header>Rick And Morty Characters</Header>
+        <CharContainer>
+          {
+            chars.map(char => {
+              return <Characters info={char} />
+            })
+          }
+        </CharContainer>
+      </MainContain>
     </div>
   );
 }
 
 export default App;
+
+const MainContain = styled.div`
+margin: auto;
+`
+
+const CharContainer = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+align-items: center;
+`
+
+const Header = styled.h1`
+font-size: 3.6rem;
+padding-top: 2%;
+`
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,63 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+export default function Characters(props) {
+    const { info } = props;
+
+    return (    
+        <MainContain>
+            <CharacterContainer>
+                <CharacterImg src={props.info.image} alt={props.info.name}></CharacterImg>
+                 <InfoContainer>
+                    <StyledPara>Name: {props.info.name}</StyledPara>
+                    <StyledPara>Status: {props.info.status}</StyledPara>
+                    <StyledPara>Species: {props.info.species}</StyledPara>
+                    <StyledPara>Gender: {props.info.gender}</StyledPara>
+                    <StyledPara>Location: {props.info.location.name}</StyledPara>
+                </InfoContainer>
+            </CharacterContainer>
+        </MainContain>
+    )
+}
+
+const MainContain = styled.div`
+margin: 1.5% 0;
+border: 1px solid red;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+align-items: center;
+width: 20%;
+min-width: 320px;
+height: 500px;
+background-image: url('https://i.dlpng.com/static/png/6727909_preview.png');
+background-position: center;
+background-size: auto;
+`
+
+const CharacterImg = styled.img`
+max-width:100%;
+`
+
+const CharacterContainer = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+align-items: center;
+margin: 2.5% 0;
+border: 1px solid red;
+width: 80%
+`
+
+const InfoContainer = styled.div`
+width: 100%;
+`
+
+const StyledPara = styled.p`
+text-align: left;
+font-weight: bold;
+`
